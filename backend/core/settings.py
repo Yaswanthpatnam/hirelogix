851cae9335070c593e7164eaf8891d6424e91256
Yaswanth babu Patnam <yaswanthpatnam1819@gmail.com>
2026-01-07 16:22:58 +0530

    updated settings.py

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -191,9 +191,11 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://hirelogix.vercel.app",  
+    "http://localhost:5173/",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://hirelogix.onrender.com",
     "https://hirelogix.vercel.app",
+    "http://localhost:5173/",
 ]
